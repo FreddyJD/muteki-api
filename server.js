ecad1a5b6ac26c31187d12293f22d3df8d837a12
Freddy Diaz <fbalzan20@gmail.com>
2023-04-14 19:33:44 -0400

    html pathing removed

--- server.js
+++ server.js
@@ -5,21 +5,23 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public', {
+    extensions: ['html', 'htm']
+}));
 
 app.get('/api/ads', (req, res) => {
-  const adFolderPath = path.join(__dirname, 'public', 'ads');
-  fs.readdir(adFolderPath, (err, files) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error reading ad folder');
-      return;
-    }
-    const imageURLs = files.map(file => `/ads/${file}`);
-    res.json(imageURLs);
-  });
+    const adFolderPath = path.join(__dirname, 'public', 'ads');
+    fs.readdir(adFolderPath, (err, files) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error reading ad folder');
+            return;
+        }
+        const imageURLs = files.map(file => `/ads/${file}`);
+        res.json(imageURLs);
+    });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });

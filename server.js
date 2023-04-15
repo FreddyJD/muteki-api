e43f6be2696a89095f9d567363302e1467e9e9df
Freddy Diaz <fbalzan20@gmail.com>
2023-04-15 05:02:41 -0400

    added state

--- server.js
+++ server.js
@@ -6,8 +6,26 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public', {
-    extensions: ['html', 'htm']
+    // extensions: ['html', 'htm']
 }));
+
+// Temporary state to store active streamers
+const activeStreamers = new Set();
+
+app.get('/creators', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'setup_error.html'));
+});
+
+
+app.get('/creators/:streamerName', (req, res) => {
+    const streamerName = req.params.streamerName;
+
+    // Add streamerName to the activeStreamers set
+    activeStreamers.add(streamerName);
+
+    // Serve the creators.html file
+    res.sendFile(path.join(__dirname, 'public', 'creators.html'));
+});
 
 app.get('/api/ads', (req, res) => {
     const adFolderPath = path.join(__dirname, 'public', 'ads');
@@ -20,6 +38,10 @@ app.get('/api/ads', (req, res) => {
         const imageURLs = files.map(file => `/ads/${file}`);
         res.json(imageURLs);
     });
+});
+
+app.get('/api/active_streamers', (req, res) => {
+    res.json(Array.from(activeStreamers));
 });
 
 app.listen(PORT, () => {
